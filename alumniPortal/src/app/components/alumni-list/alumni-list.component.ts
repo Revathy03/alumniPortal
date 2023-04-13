84161a1b92631b1467c57002ba7e46ee13be676a
REVATHY <90612776+Revathy03@users.noreply.github.com>
2023-04-13 12:33:54 +0530

    main

---
+++ alumniPortal/src/app/components/alumni-list/alumni-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumni-list',
+  templateUrl: './alumni-list.component.html',
+  styleUrls: ['./alumni-list.component.css']
+})
+export class AlumniListComponent {
+
+}
