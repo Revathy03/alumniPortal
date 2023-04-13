fca21ff28f142e9a9cd692b580ce60c74e503b7b
REVATHY <90612776+Revathy03@users.noreply.github.com>
2023-04-13 14:51:56 +0530

    angular update

--- alumniPortal/src/app/app-routing.module.ts
+++ alumniPortal/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { SignupComponent } from './components/signup/signup.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { EventsComponent } from './components/events/events.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { AlumniDashboardComponent } from './components/alumni-dashboard/alumni-dashboard.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 
 const routes: Routes = [
   {
@@ -30,6 +33,18 @@ const routes: Routes = [
 {
   path:'contact', 
   component:ContactComponent
+},
+{
+  path:'adm-dashboard', 
+  component:AdminDashboardComponent
+},
+{
+  path:'dashboard', 
+  component:AlumniDashboardComponent
+},
+{
+  path:'profile', 
+  component:EditProfileComponent
 },
 ];
 
